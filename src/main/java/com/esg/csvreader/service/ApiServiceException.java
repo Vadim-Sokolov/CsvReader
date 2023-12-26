46df3197f859e72d61189384642357bd45c523d3
Vadim-Sokolov <vadim.sokolov@gmail.com>
2023-12-26 11:06:32 +0000

    ApiServiceImpl and tests

---
+++ src/main/java/com/esg/csvreader/service/ApiServiceException.java
@@ -0,0 +1,7 @@
+package com.esg.csvreader.service;
+
+public class ApiServiceException extends Exception {
+    public ApiServiceException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/esg/csvreader/service/ApiServiceImpl.java
+++ src/main/java/com/esg/csvreader/service/ApiServiceImpl.java
@@ -1,9 +1,7 @@
 package com.esg.csvreader.service;
 
 import com.esg.csvreader.PropertyLoader;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -11,6 +9,7 @@ import java.util.List;
 public class ApiServiceImpl {
 
     private static final String POST_URL = PropertyLoader.PROPERTIES.getProperty("postEndPoint");
+    private static final String GET_URL = PropertyLoader.PROPERTIES.getProperty("getEndPoint");
     private final RestTemplate restTemplate;
 
     public ApiServiceImpl(RestTemplate restTemplate) {
@@ -26,5 +25,19 @@ public class ApiServiceImpl {
 
             restTemplate.postForObject(POST_URL, requestEntity, String.class);
         });
+    }
+
+    public String getCustomerInformationByReferenceNumber(Integer refNumber) throws ApiServiceException {
+
+        var apiUrlWithId = GET_URL + "/{" + refNumber + "}";
+
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                apiUrlWithId, HttpMethod.GET, null, String.class);
+
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            return responseEntity.getBody();
+        } else {
+            throw new ApiServiceException("Failed to retrieve record. HTTP Status: " + responseEntity.getStatusCode());
+        }
     }
 }
